f246ce08d3f53b4c733fbffbe6266b9bb0569ead
Brandon <74200568+bsalinassanchez@users.noreply.github.com>
2023-05-28 18:30:17 -0700

    my work for lab 8

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Lab 8 - Starter
+
+# Brandon Salinas Sanchez
+
+# A16760692
+
+1. Personally, I would fit my automated tests in a Github action that runs whenever the code is pushed. If the code fails some critical tests, then the push is flagged. My team has setup some github action that prevents a pr to be merged if some tests fail in order to ensure that the main branch is up to date and works.
+
+2. No, becuase E2E tests are meant to test what a user would experience if they interacted with our website and not what is returned by some function.
+
+3. I think it depends on what exactly you would want to use the unit test _to test_. For example, If I wanted to test whether or not the message sent over was correct (i.e. a string, specific value, etc), then I could test that in a unit test. But If I wanted to test whether or not the message was sent from me, and received by another user, then unit test would not be the way to go.
+
+4. for testing a "max message length" feature then I would consider using unit testing. If a string is passed into the function, and the function returns a boolean where true is valid and false is too long, then it would be easy enough to create unit tests for this function.
+
+## Screenshots of tests passing
+
+![sum-unit-tests](sum-unit-tests.png)
+![lab8-tests](lab8-tests.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,130 @@
 // unit.test.js
 
-const functions = require('../code-to-unit-test/unit-test-me.js');
+const functions = require("../code-to-unit-test/unit-test-me.js");
 
 // TODO - Part 2
+
+// isPhoneNumber tests
+
+test("testing a normal phone number", () => {
+	const phoneNumber = "(760) 443-1234";
+	let result = functions.isPhoneNumber(phoneNumber);
+	expect(result).toBe(true);
+});
+
+test("testing another normal phone number", () => {
+	const phoneNumber = "760-123-3253";
+	let result = functions.isPhoneNumber(phoneNumber);
+	expect(result).toBe(true);
+});
+
+test("testing a bad format phone number", () => {
+	const phoneNumber = "619 234 1923";
+	let result = functions.isPhoneNumber(phoneNumber);
+	expect(result).toBe(false);
+});
+
+test("testing a bad format phone number", () => {
+	const phoneNumber = "1252231439";
+	let result = functions.isPhoneNumber(phoneNumber);
+	expect(result).toBe(false);
+});
+
+//testing isEmail
+
+test("testing a good format email", () => {
+	const email = "test123@gmail.com";
+	let result = functions.isEmail(email);
+	expect(result).toBe(true);
+});
+test("testing a good format email", () => {
+	const email = "123test@yahoo.com";
+	let result = functions.isEmail(email);
+	expect(result).toBe(true);
+});
+test("testing a bad format email", () => {
+	const email = "!%$%@email.com";
+	let result = functions.isEmail(email);
+	expect(result).toBe(false);
+});
+
+test("testing a bad format email", () => {
+	const email = "test123@gm!ail.com";
+	let result = functions.isEmail(email);
+	expect(result).toBe(false);
+});
+
+//testing is password
+
+test("testing a good format password", () => {
+	const pw = "HI_123_HAHA";
+	let result = functions.isStrongPassword(pw);
+	expect(result).toBe(true);
+});
+test("testing a good format password", () => {
+	const pw = "mypw1234_";
+	let result = functions.isStrongPassword(pw);
+	expect(result).toBe(true);
+});
+test("testing a good format password", () => {
+	const pw = "_secret123";
+	let result = functions.isStrongPassword(pw);
+	expect(result).toBe(false);
+});
+test("testing a good format password", () => {
+	const pw = "32!@#test";
+	let result = functions.isStrongPassword(pw);
+	expect(result).toBe(false);
+});
+
+// testing isDate
+
+test("testing a good date format date", () => {
+	const date = "12/31/2023";
+	let result = functions.isDate(date);
+	expect(result).toBe(true);
+});
+
+test("testing another good date format date", () => {
+	const date = "1/1/2000";
+	let result = functions.isDate(date);
+	expect(result).toBe(true);
+});
+
+test("testing a bad date format date", () => {
+	const date = "31/12/22";
+	let result = functions.isDate(date);
+	expect(result).toBe(false);
+});
+
+test("testing another bad date format date", () => {
+	const date = "2023/12/31";
+	let result = functions.isDate(date);
+	expect(result).toBe(false);
+});
+
+// testing isHexColor
+
+test("testing a good hex color format hex color", () => {
+	const color = "#123abc";
+	let result = functions.isHexColor(color);
+	expect(result).toBe(true);
+});
+
+test("testing another good hex color format hex color", () => {
+	const color = "#abc";
+	let result = functions.isHexColor(color);
+	expect(result).toBe(true);
+});
+
+test("testing a bad hex color format hex color", () => {
+	const color = "#12345";
+	let result = functions.isHexColor(color);
+	expect(result).toBe(false);
+});
+
+test("testing another bad hex color format hex color", () => {
+	const color = "#abcd";
+	let result = functions.isHexColor(color);
+	expect(result).toBe(false);
+});
